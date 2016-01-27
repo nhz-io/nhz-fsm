9e35d889485ed45c675cd7b02bc4fbeb58217fa1
Ishi Ruy <dev@nhz.io>
2016-01-28 01:49:12 +0200

    FSM-40 #commment made the machine list selectable

--- dev/MachinesList.jsx
+++ dev/MachinesList.jsx
@@ -5,6 +5,8 @@ import ListItem from 'material-ui/lib/lists/list-item';
 import Avatar from 'material-ui/lib/avatar';
 import MemoryIcon from 'material-ui/lib/svg-icons/hardware/memory';
 import ClearIcon from 'material-ui/lib/svg-icons/content/clear';
+import {SelectableContainerEnhance} from 'material-ui/lib/hoc/selectable-enhance';
+let SelectableList = SelectableContainerEnhance(List);
 
 export default class MachinesList extends Base {
   constructor(props) {
@@ -14,35 +16,34 @@ export default class MachinesList extends Base {
   render() {
     const { props } = this;
     return(
-      <List {...props}>
+      <SelectableList {...props}>
         {
           Object
-            .keys(props.machines)
-            .map((key) => ({id:key, machine:props.machines[key]}))
-            .sort((a, b) => a.machine.name > b.machine.name)
-            .map((item) => {
-              let date = new Date(item.machine.timestamp);
-              return(
-                <ListItem
-                  key={item.id}
-                  leftAvatar={<Avatar icon={<MemoryIcon />} />}
-                  rightIcon={
-                    <ClearIcon
-                      onClick={() => {
-                        props.onDeleteAction && props.onDeleteAction(item);
-                      }}
-                      onTouchTap={() => {
-                        props.onDeleteAction && props.onDeleteAction(item);
-                      }}
-                    />
-                  }
-                  primaryText={item.machine.name}
-                  secondaryText={`${date.toLocaleDateString()} ${date.toLocaleTimeString()}`}
+          .keys(props.machines)
+          .map((key) => ({id:key, machine:props.machines[key]}))
+          .sort((a, b) => a.machine.name > b.machine.name)
+          .map((item) => {
+          let date = new Date(item.machine.timestamp);
+          return(
+            <ListItem
+              value={item.id}
+              key={item.id}
+              leftAvatar={<Avatar icon={<MemoryIcon />} />}
+              rightIcon={
+                <ClearIcon
+                  onTouchTap={(e) => {
+                    e.stopPropagation();
+                    props.onDeleteAction && props.onDeleteAction(item);
+                  }}
                 />
-              );
-            })
+              }
+              primaryText={item.machine.name}
+              secondaryText={`${date.toLocaleDateString()} ${date.toLocaleTimeString()}`}
+            />
+            );
+          })
         }
-      </List>
+      </SelectableList>
     );
   }
 }
