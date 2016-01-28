c50a54bdc164e0ca9de659ac1dca12969b624b2f
Ishi Ruy <dev@nhz.io>
2016-01-29 00:32:36 +0200

    FSM-52 #comment extended _isChildSelected()

--- dev/components/SelectableContainerEnhance.jsx
+++ dev/components/SelectableContainerEnhance.jsx
@@ -1,6 +1,13 @@
 import SelectableEnhance from 'material-ui/lib/hoc/selectable-enhance';
 
 export default class SelectableContainerEnhance extends SelectableEnhance {
+  _isChildSelected(child, props) {
+    let childValue = child.props.value;
+    let itemValue = this.getValueLink(props).value.find(childValue);
+
+    return (itemValue && itemValue === childValue);
+  }
+
   getValueLink(props) {
     return props.valueLink || {
       value: props.value || [],
