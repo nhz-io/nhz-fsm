fe88dd93b41682b9677c1dac54b7899c1df8e863
Ishi Ruy <dev@nhz.io>
2016-01-29 02:00:58 +0200

    FSM-52 #comment had to wipe out everything and use the enhancer from material-ui

--- dev/components/SelectableContainerEnhance.jsx
+++ dev/components/SelectableContainerEnhance.jsx
@@ -1,26 +1,127 @@
+import React from 'react';
+import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
+import StylePropable from 'material-ui/lib/mixins/style-propable';
+import ColorManipulator from 'material-ui/lib/utils/color-manipulator';
 import SelectableEnhance from 'material-ui/lib/hoc/selectable-enhance';
 
-export default class SelectableContainerEnhance extends SelectableEnhance {
-  _isChildSelected(child, props) {
-    let childValue = child.props.value;
-    let itemValue = this.getValueLink(props).value.find(childValue);
+export default function SelectableContainerEnhance(Component) {
+  const composed = React.createClass({
 
-    return (itemValue && itemValue === childValue);
-  }
+    displayName: `Selectable${Component.displayName}`,
 
-  getValueLink(props) {
-    return props.valueLink || {
-      value: props.value || [],
-      requestChange: props.onChange,
-    };
-  }
+    propTypes: {
+      children: React.PropTypes.node,
+      selectedItemStyle: React.PropTypes.object,
+      valueLink: React.PropTypes.shape({
+        value: React.PropTypes.any,
+        requestChange: React.PropTypes.func,
+      }).isRequired,
+    },
 
-  _handleItemTouchTap(e, item) {
-    let valueLink = this.getValueLink(this.props);
-    let itemValue = item.props.value;
-    let menuValue = valueLink.value.find(itemValue);
-    if ( itemValue !== menuValue) {
-      valueLink.requestChange(e, itemValue);
-    }
-  }
-}
+    contextTypes: {
+      muiTheme: React.PropTypes.object,
+    },
+
+    childContextTypes: {
+      muiTheme: React.PropTypes.object,
+    },
+
+    mixins: [
+      StylePropable,
+    ],
+
+    getInitialState() {
+      return {
+        muiTheme: this.context.muiTheme || getMuiTheme(),
+      };
+    },
+
+    getChildContext() {
+      return {
+        muiTheme: this.state.muiTheme,
+      };
+    },
+
+    componentWillReceiveProps(nextProps, nextContext) {
+      let newMuiTheme = nextContext.muiTheme ? nextContext.muiTheme : this.state.muiTheme;
+      this.setState({muiTheme: newMuiTheme});
+    },
+
+    getValueLink(props) {
+      return props.valueLink || {
+        value: props.value || [],
+        requestChange: props.onChange,
+      };
+    },
+
+    _extendChild(child, styles, selectedItemStyle) {
+      if (child && child.type && child.type.displayName === 'ListItem') {
+        let selected = this._isChildSelected(child, this.props);
+        let selectedChildrenStyles = {};
+        if (selected) {
+          selectedChildrenStyles = this.mergeStyles(styles, selectedItemStyle);
+        }
+
+        let mergedChildrenStyles = this.mergeStyles(child.props.style || {}, selectedChildrenStyles);
+
+        this._keyIndex += 1;
+
+        return React.cloneElement(child, {
+          onTouchTap: (e) => {
+            this._handleItemTouchTap(e, child);
+            if (child.props.onTouchTap) {
+              child.props.onTouchTap(e);
+            }
+          },
+          key: this._keyIndex,
+          style: mergedChildrenStyles,
+          nestedItems: child.props.nestedItems.map((child) => this._extendChild(child, styles, selectedItemStyle)),
+        });
+      } else {
+        return child;
+      }
+    },
+
+    _isChildSelected(child, props) {
+      let childValue = child.props.value;
+      let itemValue = this.getValueLink(props).value.find(childValue);
+
+      return (itemValue && itemValue === childValue);
+    },
+
+    _handleItemTouchTap(e, item) {
+      let valueLink = this.getValueLink(this.props);
+      let itemValue = item.props.value;
+      let menuValue = valueLink.value.find(itemValue);
+      if ( itemValue !== menuValue) {
+        valueLink.requestChange(e, itemValue);
+      }
+    },
+
+    render() {
+      const {children, selectedItemStyle} = this.props;
+      this._keyIndex = 0;
+      let styles = {};
+
+      if (!selectedItemStyle) {
+        let textColor = this.state.muiTheme.rawTheme.palette.textColor;
+        let selectedColor = ColorManipulator.fade(textColor, 0.2);
+        styles = {
+          backgroundColor: selectedColor,
+        };
+      }
+
+      let newChildren = React.Children.map(children, (child) => this._extendChild(child, styles, selectedItemStyle));
+
+      return (
+        <Component {...this.props} {...this.state}>
+          {newChildren}
+        </Component>
+      );
+    },
+  });
+
+  return composed;
+};
+
+export default SelectableContainerEnhance;
