84a4d8b57402a2a1c3d33d61688672e078e4f043
Ishi Ruy <dev@nhz.io>
2016-01-29 21:07:53 +0200

    FSM-62 #comment created FinalStateList component

---
+++ dev/components/FinalStateList.jsx
@@ -0,0 +1,38 @@
+import SelectableContainerEnhance from './SelectableContainerEnhance';
+import ListItem from 'material-ui/lib/lists/list-item';
+import React from 'react';
+const { Component, PropTypes } = React;
+import SelectableList from './SelectableList';
+
+export default class FinalStateList extends Component {
+
+  static propTypes = {
+    states        : PropTypes.array,
+    selected      : PropTypes.string,
+    mask          : PropTypes.array,
+    requestChange : PropTypes.func,
+  };
+
+  render() {
+    const { props } = this;
+    const { states, selected, requestChange } = props;
+    return(
+      <SelectableList
+        {...props}
+        valueLink={{
+          value         : selected,
+          mask          : mask,
+          requestChange : requestChange,
+        }}
+      >
+        {(states || []).map((state) => {
+          return(
+            <ListItem value={state} key={state}>
+              <h3 style={{margin:2}}>{state}</h3>
+            </ListItem>
+          );
+        })}
+      </SelectableList>
+    );
+  }
+}
