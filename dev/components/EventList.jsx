714a584648733076a8e4ed851e11f61e98e6d7a1
Ishi Ruy <dev@nhz.io>
2016-01-29 20:59:18 +0200

    FSM-61 #comment created EventListComponent

---
+++ dev/components/EventList.jsx
@@ -0,0 +1,38 @@
+import SelectableContainerEnhance from './SelectableContainerEnhance';
+import ListItem from 'material-ui/lib/lists/list-item';
+import React from 'react';
+const { Component, PropTypes } = React;
+import SelectableList from './SelectableList';
+
+export default class EventList extends Component {
+
+  static propTypes = {
+    events        : PropTypes.array,
+    selected      : PropTypes.string,
+    mask          : PropTypes.array,
+    requestChange : PropTypes.func,
+  };
+
+  render() {
+    const { props } = this;
+    const { events, selected, requestChange } = props;
+    return(
+      <SelectableList
+        {...props}
+        valueLink={{
+          value         : selected,
+          mask          : mask,
+          requestChange : requestChange,
+        }}
+      >
+        {(events || []).map((event) => {
+          return(
+            <ListItem value={event} key={event}>
+              <h3 style={{margin:2}}>{event}</h3>
+            </ListItem>
+          );
+        })}
+      </SelectableList>
+    );
+  }
+}
